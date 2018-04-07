f700b96796286c45456a4688db634bad67baafaf
DESKTOP-4SDMB7R\p78o2 <p78o233@126.com>
2018-04-07 09:29:23 +0800

    2018040701

--- src/main/java/com/example/demo/controller/WebSocket.java
+++ src/main/java/com/example/demo/controller/WebSocket.java
@@ -7,20 +7,25 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  10  * @ServerEndpoint 注解是一个类层次的注解，它的功能主要是将目前的类定义成一个websocket服务器端,
  11  * 注解的值将被用于监听用户连接的终端访问URL地址,客户端可以通过这个URL来连接到WebSocket服务器端
  12  */
-@ServerEndpoint(value = "/websocket")
+//{param}用于接收创建链接的时候接收前端页面传递过来的参数
+@ServerEndpoint(value = "/websocket/{param}")
 @Component
 public class WebSocket {
 //    静态变量用于记录在线人数
     private static  int onLineCount = 0;
-
+//    用于保存用户信息  key用户ID  value sessionID
+    private static Map<String,String> userMap = new HashMap<String,String>();
     private static CopyOnWriteArraySet<WebSocket> webSocketSet = new CopyOnWriteArraySet<WebSocket>();
 
 //    与某个客户端连接会话，需要通过它来发送数据
@@ -29,12 +34,14 @@ public class WebSocket {
 //    成功建立的调用方法
 //    session参数可选
     @OnOpen
-    public void onOpen(Session session)
+    public void onOpen(@PathParam(value="param") String param, Session session)
     {
         this.session = session;
         webSocketSet.add(this);
         addOnlineCount();
         System.out.println(getOnlineCount());
+//        保存用户信息
+        userMap.put(param,session.getId());
     }
 //    连接关闭
     @OnClose
@@ -53,7 +60,9 @@ public class WebSocket {
         for(WebSocket item : webSocketSet){
             try{
 //                发送消息的方法
-                item.sendMessage(messsage);
+                if(session.getId().equals(item.session.getId())) {
+                    item.sendMessage(messsage);
+                }
             }
             catch (IOException e){
                 e.printStackTrace();
@@ -70,14 +79,17 @@ public class WebSocket {
     public void sendMessage(String message) throws IOException{
         this.session.getBasicRemote().sendText(message);
     }
+//    增加在线人数
     public void addOnlineCount()
     {
         WebSocket.onLineCount++;
     }
+//    减少在线人数
     public void sunOnlineCount()
     {
         WebSocket.onLineCount--;
     }
+//    获取当前在线人数
     public int getOnlineCount(){
         return WebSocket.onLineCount;
     }
